665ba8856a3b225a6556fc8e646084a60b3cdc17
Adowrath <cyrill.brunner@hispeed.ch>
2021-01-09 12:45:45 +0100

    Non-working pause screen

---
+++ Assets/Scripts/PauseMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtons : MonoBehaviour {
+    public void RestartGame() {
+        Debug.Log("RestartGame");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame() {
+        Debug.Log("QuitGame");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ToggleOnEscape.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ToggleOnEscape : MonoBehaviour {
+
+    public GameObject toggledElement;
+    public bool initiallyVisible = false;
+
+    private bool currentlyVisible;
+
+    // Start is called before the first frame update
+    void Start() {
+        toggledElement.SetActive(initiallyVisible);
+        currentlyVisible = initiallyVisible;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+            toggledElement.SetActive(currentlyVisible = !currentlyVisible);
+        }
+    }
+}
